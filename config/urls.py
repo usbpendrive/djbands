7fcca8759bf0ac781d46bb26bfca43f1d6a5b777
usbpendrive <ferry.fernando@gmail.com>
2025-12-14 07:21:04 +0700

    basic views

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home import views as home_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('credits/', home_views.credits),
 ]
