b73d0a94b53bb9143fbf01f34fb4069bc70400f1
usbpendrive <ferry.fernando@gmail.com>
2025-12-14 07:45:20 +0700

    Template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def credits(request):
     content = {"Nicky\nYour Name"}
 
     return HttpResponse(content, content_type="text/plain")
+
+def news(request):
+    data = {
+        'news': [
+            "DJBand now has a news page!",
+            "DJBand has its first web page",
+        ]
+    }
+
+    return render(request, "news.xhtml", data)
